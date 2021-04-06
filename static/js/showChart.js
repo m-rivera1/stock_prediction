831905ffcb27b8145d8ba0fdb7bb136504cfa2c3
unknown <kiwwanne@gmail.com>
2021-04-06 13:22:07 -0500

    Chart hide function added

--- static/js/showChart.js
+++ static/js/showChart.js
@@ -1,23 +1,30 @@
-function showInitialChart(chartData1){
-var ctx = document.getElementById('stockchart').getContext('2d');
-var stockchart = new Chart(ctx, {
-    type: 'line',
-    data: {
-        datasets: [{
-            label: "Test",
-            data: chartData1
-        }]
-    },
-    options: {
-        responsive: true,
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true,
-                    stepSize: 50
-                }
-            }]
-        }
+var myVar;
+
+function displycharts() {
+    if (window.location.href.includes('stock-analysis')) {
+        document.getElementById('chart2').style.display = "display";
+        document.getElementById('chart1').style.display = "none";
+        document.getElementById('analysis-btn').style.display = "none";
     }
-});
+    else {
+        document.getElementById('chart2').style.display = "none";
+        document.getElementById('chart1').style.display = "display";
+        document.getElementById('analysis-btn').style.display = "display";
+    }
 }
+
+
+
+
+function loaderhide() {
+    document.getElementById("loader").style.display = "none";
+}
+
+// function myFunction() {
+//     myVar = setTimeout(showPage, 3000);
+// }
+
+// function showPage() {
+//     document.getElementById("loader").style.display = "none";
+//     document.getElementById("analysischart").style.display = "block";
+// }
